588a98022aab8d81241c3ade4454d253e09c1d19
Myer921 <icecat@koishi.moe>
2024-02-14 17:11:22 +0000

    重新赋值变量

--- main_v2.py
+++ main_v2.py
@@ -36,16 +36,18 @@ import traceback
 load_dotenv()
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 TTS_API_PATH = os.getenv("TTS_API_PATH")
-TTS_API_LANGUAGE = os.getenv("TTS_API_LANGUAGE")
-TTS_API_TOPK = os.getenv("TTS_API_TOPK")       # top_k设置为变量，方便调整
-VERSION = "0.2.4-test"
+TTS_API_LANGUAGE = os.getenv("TTS_API_LANGUAGE", "auto")
+TTS_API_TOPK = os.getenv("TTS_API_TOPK","20")
+TTS_API_TOPP = os.getenv("TTS_API_TOPP", "0.6")
+TTS_API_temperature = os.getenv("TTS_API_temperature","0.6")
+VERSION = "0.2.7"
 ALLOWED_IDS = [int(i) for i in os.getenv("ALLOWED_IDS", "").split(",") if i]
 ALLOWED_USER_IDS = [int(i) for i in os.getenv("ALLOWED_USER_IDS", "").split(",") if i]
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 OPENAI_PROMPT_ROLE = os.getenv("OPENAI_PROMPT_ROLE", "AI助手")
-OPENAI_ENGINE = os.getenv("OPENAI_ENGINE", "davinci")
+OPENAI_ENGINE = os.getenv("OPENAI_ENGINE", "gpt-3.5-turbo")
 OPENAI_API_BASE = os.getenv("OPENAI_API_BASE", "https://api.openai.com")
-MOEW_NAME = os.getenv("MOEW_NAME")
+MOEW_NAME = os.getenv("MOEW_NAME", "香草")
 
 # 加载 openai 组件
 client = OpenAI(api_key=OPENAI_API_KEY)
@@ -290,6 +292,8 @@ async def start_tts_task(context: ContextTypes.DEFAULT_TYPE):
                 "lang": TTS_API_LANGUAGE,
                 "preset": "default",
                 "top_k": TTS_API_TOPK,
+                "top_p": TTS_API_TOPP,
+                "temperature": TTS_API_temperature,
                 "text": job.text,
             }
             query_string = urllib.parse.urlencode(params)
